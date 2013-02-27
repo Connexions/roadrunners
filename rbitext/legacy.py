8d33657b6d65e2988af720d128afc1b2b4d1545b
Michael Mulich <michael.mulich@gmail.com>
2013-02-26 22:56:23 -0500

    A draft for the legacy request handler for an offline zip.

--- rbitext/legacy.py
+++ rbitext/legacy.py
@@ -10,10 +10,15 @@ Public License Version 2.1 (LGPL).  See LICENSE.txt for details.
 """
 import os
 import sys
+import tempfile
+import traceback
+import subprocess
+import shutil
 import jsonpickle
 import requests
 
-from .utils import logger
+from rbit import Blocked
+from .utils import logger, get_completezip, unpack_zip
 
 
 def make_completezip(message, set_status, settings={}):
@@ -63,5 +68,91 @@ def make_completezip(message, set_status, settings={}):
     set_status('Building', "Placing file a location: " + output_filepath)
     with open(output_filepath, 'wb') as f:
         f.write(resp.content)
+
+    set_status('Done')
+
+
+def make_offlinezip(message, set_status, settings={}):
+    """\
+    Creates an offlinezip using the complete zip (dependency).
+
+    Available settings:
+
+    - **output-dir** - Directory where the produced file is stuck.
+    - **oer.exports-dir** - Defines the location of the oer.exports package.
+    - **python** - Defines which python executable should be used.
+
+    """
+    output_dir = settings['output-dir']
+    python_executable = settings.get('python', sys.executable)
+    oerexports_dir = settings['oer.exports-dir']
+
+    # Start the building sequence by updating the build's status.
+    build_request = jsonpickle.decode(message)
+    build_request.stamp_request()
+    timestamp = build_request.get_buildstamp()
+    status_message = "Starting job, timestamp: {0}".format(timestamp)
+    set_status('Building', status_message)
+
+    # Acquire the collection's data in a collection directory format.
+    id = build_request.get_package()
+    version = build_request.get_version()
+    # This should be something like 'http://cnx.org:80'.
+    base_uri = build_request.transport.uri.rstrip('/')
+
+    # Create a temporary directory to work in...
+    build_dir = tempfile.mkdtemp()
+    logger.debug("Working in '{0}'.".format(build_dir))
+
+    # Acquire the completezip file for use in the build. The
+    #   completezip could be in the output directory. If it's not
+    #   there we will need to download it from the host repository.
+    completezip_filename = '{0}-{1}.complete.zip'.format(id, version)
+    completezip_filepath = os.path.join(output_dir, completezip_filename)
+    if os.path.exists(completezip_filepath):
+        content_dir = unpack_zip(completezip_filepath, build_dir)[0]
+    else:
+        # Looks like we will need to download the file...
+        try:
+            content_dir = get_completezip(id, version, base_uri, build_dir)
+        except Exception as exc:
+            traceback.print_exc(exc)
+            raise Blocked("Issues is probably that the complete zip "
+                          "does not exist yet.")
+
+    # Run the oer.exports script against the collection data.
+    build_script = os.path.join(oerexports_dir, 'content2epub.py')
+    result_filename = '{0}-{1}.offline.zip'.format(id, version)
+    result_filepath = os.path.join(build_dir, result_filename)
+    command = [python_executable, build_script, content_dir,
+               # The follow are not optional, values must be supplied.
+               '-t', 'collection',
+               '-c', os.path.join(oerexports_dir, 'static',
+                                  'offline-zip-overrides.css'),
+               '-e', os.path.join(oerexports_dir, 'xsl', 'dbk2html.xsl'),
+               '-o', result_filepath,
+               ]
+    set_status('Building', "Running: " + ' '.join(command))
+    process = subprocess.Popen(command,
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                               cwd=build_dir)
+    stdout, stderr = process.communicate()
+    if process.returncode != 0:
+        # Something went wrong...
+        set_status('Failed', stderr)
+        return
+    else:
+        msg = "Offline zip created, moving contents to final destination..."
+        set_status('Building', msg)
+
+    # TODO Add a readme file to the zip Also, remove epub metadata files.
+
+    # Write out the results to the filesystem.
+    shutil.copy2(result_filepath, output_dir)
+    output_filepath = os.path.join(output_dir, result_filename)
+    set_status('Building', "Placing file a location: " + output_filepath)
+
+    # Remove the temporary build directory
+    shutil.rmtree(build_dir)
 
     set_status('Done')
