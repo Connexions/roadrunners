3087eca5a9df25ee3a8ce0eac34b058e0327bfde
Michael Mulich <michael.mulich@gmail.com>
2013-03-07 13:05:08 -0800

    Use the cnx-buildout script that builds both the offlinezip and the epub. We are using this script because the layer removal turned out to be more difficult than expected, mostly due to unclear system dependency requirements.

--- rbitext/legacy.py
+++ rbitext/legacy.py
@@ -80,12 +80,22 @@ def make_offlinezip(message, set_status, settings={}):
 
     - **output-dir** - Directory where the produced file is stuck.
     - **oer.exports-dir** - Defines the location of the oer.exports package.
-    - **python** - Defines which python executable should be used.
+    - **cnx-buildout-dir** - Defines the location of cnx-buildout.
+    - **python-env** - Defines a virtual-env directory to activate for this
+      legacy stuff.
+
+    Dependencies:
+
+    - Requires an active virtual environment or that the python dependencies
+      have been met by the system python.
+    - Requires a copy of oer.exports and cnx-buildout, both of which have
+      scripts in them that this will utilize.
 
     """
     output_dir = settings['output-dir']
-    python_executable = settings.get('python', sys.executable)
     oerexports_dir = settings['oer.exports-dir']
+    cnxbuildout_dir = settings['cnx-buildout-dir']
+    python_env = settings.get('python-env', None)
 
     # Start the building sequence by updating the build's status.
     build_request = jsonpickle.decode(message)
@@ -109,33 +119,52 @@ def make_offlinezip(message, set_status, settings={}):
     #   there we will need to download it from the host repository.
     completezip_filename = '{0}-{1}.complete.zip'.format(id, version)
     completezip_filepath = os.path.join(output_dir, completezip_filename)
-    if os.path.exists(completezip_filepath):
-        content_dir = unpack_zip(completezip_filepath, build_dir)[0]
-    else:
+    if not os.path.exists(completezip_filepath):
         # Looks like we will need to download the file...
         try:
-            content_dir = get_completezip(id, version, base_uri, build_dir)
+            completezip_filepath = get_completezip(id, version,
+                                                   base_uri, build_dir,
+                                                   unpack=False)
         except Exception as exc:
             traceback.print_exc(exc)
             raise Blocked("Issues is probably that the complete zip "
                           "does not exist yet.")
+    else:
+        # The build script needs working directory access to the
+        #   complete zip file.
+        shutil.copy2(completezip_filepath, build_dir)
 
     # Run the oer.exports script against the collection data.
-    build_script = os.path.join(oerexports_dir, 'content2epub.py')
-    result_filename = '{0}-{1}.offline.zip'.format(id, version)
-    result_filepath = os.path.join(build_dir, result_filename)
-    command = [python_executable, build_script, content_dir,
-               # The follow are not optional, values must be supplied.
-               '-t', 'collection',
-               '-c', os.path.join(oerexports_dir, 'static',
-                                  'offline-zip-overrides.css'),
-               '-e', os.path.join(oerexports_dir, 'xsl', 'dbk2html.xsl'),
-               '-o', result_filepath,
-               ]
+    build_script = os.path.join(cnxbuildout_dir, 'scripts',
+                                'content2epub.bash')
+    offlinezip_result_filename = '{0}-{1}.offline.zip'.format(id, version)
+    # XXX The build script putting the file somewhere other other than where
+    #     I told it to. *Grumbles*
+    unpacked_collection_dir = "{0}_{1}_complete".format(id, version)
+    offlinezip_result_filepath = os.path.join(build_dir,
+                                              unpacked_collection_dir,
+                                              offlinezip_result_filename)
+    epub_result_filename = '{0}-{1}.epub'.format(id, version)
+    epub_result_filepath = os.path.join(build_dir, epub_result_filename)
+
+    command = []
+    working_dir = build_dir
+    if python_env is not None:
+        activate = os.path.join(python_env, 'bin', 'activate')
+        command.extend(['source', activate, '&&'])
+        working_dir = python_env
+    command.extend([build_script, "Connexions", id, version,
+                    build_dir,  # maps to working directory
+                    completezip_filename,
+                    offlinezip_result_filename,
+                    epub_result_filename,
+                    oerexports_dir,
+                    ])
     set_status('Building', "Running: " + ' '.join(command))
-    process = subprocess.Popen(command,
+    process = subprocess.Popen(' '.join(command),
                                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                               cwd=build_dir)
+                               cwd=oerexports_dir, shell=True,
+                               executable='/bin/bash')
     stdout, stderr = process.communicate()
     if process.returncode != 0:
         # Something went wrong...
@@ -145,12 +174,17 @@ def make_offlinezip(message, set_status, settings={}):
         msg = "Offline zip created, moving contents to final destination..."
         set_status('Building', msg)
 
-    # TODO Add a readme file to the zip Also, remove epub metadata files.
-
     # Write out the results to the filesystem.
-    shutil.copy2(result_filepath, output_dir)
-    output_filepath = os.path.join(output_dir, result_filename)
-    set_status('Building', "Placing file a location: " + output_filepath)
+    offlinezip_output_filepath = os.path.join(output_dir,
+                                              offlinezip_result_filename)
+    epub_output_filepath = os.path.join(output_dir,
+                                        epub_result_filename)
+
+    msg = "Placing offline at location: {0}\nPlacing epub at location: {1}"
+    set_status('Building', msg.format(offlinezip_output_filepath,
+                                      epub_output_filepath))
+    shutil.copy2(offlinezip_result_filepath, output_dir)
+    shutil.copy2(epub_result_filepath, output_dir)
 
     # Remove the temporary build directory
     shutil.rmtree(build_dir)
