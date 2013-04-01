fd3f4591d4fb6bd26c619d552ee22b6e8ea9dd87
Michael Mulich <michael.mulich@gmail.com>
2013-04-01 08:59:31 -0700

    Revised to work with both collections and modules. Slight modifications to use the newer consumer interace and logging/exceptions for status updates.

--- rbitext/rhaptos_print.py
+++ rbitext/rhaptos_print.py
@@ -15,10 +15,12 @@ import os
 import sys
 import subprocess
 import shutil
-import jsonpickle
+
+import rbit
+from .utils import logger
 
 
-def make_print(message, set_status, settings={}):
+def make_print(build_request, settings={}):
     """rbit extension to interface with the Products.RhaptosPrint.printing
     Makefile.
 
@@ -27,7 +29,6 @@ def make_print(message, set_status, settings={}):
     - **output-dir** - Directory where the produced file is stuck.
     - **python** - Maps to the make file's PYTHON variable
     - **print-dir** - Maps to the make file's PRINT_DIR variable
-    - **host** - Maps to the make file's HOST variable
 
     """
     output_dir = settings['output-dir']
@@ -35,11 +36,10 @@ def make_print(message, set_status, settings={}):
     print_dir = settings.get('print-dir', None)
     cwd = os.path.abspath(settings.get('print-dir', os.curdir))
 
-    build_request = jsonpickle.decode(message)
     build_request.stamp_request()
     timestamp = build_request.get_buildstamp()
     status_message = "Starting job, timestamp: {0}".format(timestamp)
-    set_status('Building', status_message)
+    logger.debug(status_message)
 
     # Clean up the 'shared' environment before trying to build.
     # FIXME This is a 'shared' environment, which means we can't run
@@ -56,9 +56,12 @@ def make_print(message, set_status, settings={}):
     #     return
 
     # Run the makefile from RhaptosPrint that will create the PDF
-    content_id = build_request.get_package()
-    pdf_filename = '{0}.pdf'.format(content_id)
-    command = ['make', pdf_filename]
+    id = build_request.get_package()
+    version = build_request.get_version()
+    pdf_filename = "{}.pdf".format(id)
+    is_module = id.startswith('m')
+    make_file = is_module and 'module_print.mak' or 'course_print.mak'
+    command = ['make', '-f', make_file, pdf_filename]
     # Override various make variables.
     host = build_request.transport.uri
     host = '/'.join(host.split('/')[:3])  # just the {protocol}://{hostname}
@@ -66,24 +69,28 @@ def make_print(message, set_status, settings={}):
         python_executable and "PYTHON=" + python_executable or None,
         print_dir and "PRINT_DIR=" + print_dir or None,
         host and "HOST=" + host or None,
-        "COLLECTION_VERSION=" + build_request.job.packageinstance.package.version,
+        "VERSION=" + build_request.job.packageinstance.package.version,
         ]
     overrides = [c for c in overrides if c]  # Remove the None values.
     command.extend(overrides)
 
+    logger.debug("Running command: " + ' '.join(command))
     process = subprocess.Popen(command,
                                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                cwd=cwd)
     stdout, stderr = process.communicate()
     if process.returncode != 0:
-        # Something went wrong...
-        set_status('Failed', stderr)
-        return
+        raise rbit.Failed(stderr)
     else:
         msg = "PDF created, moving contents to final destination..."
-        set_status('Building', msg)
+        logger.debug(msg)
 
-    # Move the resulting document to the defined location.
+
+    # Rename and move the resulting document to the defined location.
+    new_pdf_filename = "{}-{}.pdf".format(id, version)
+    os.rename(os.path.join(cwd, pdf_filename),
+              os.path.join(cwd, new_pdf_filename))
+    pdf_filename = new_pdf_filename
     shutil.copy2(os.path.join(cwd, pdf_filename), output_dir)
 
-    set_status('Done')
+    return [os.path.join(output_dir, pdf_filename)]
