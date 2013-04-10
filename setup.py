8f747f1e49351a916aba0d89714c07f675edcc40
Ed Woodward <ecw1@rice.edu>
2013-04-10 13:16:58 -0500

    Added license info

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
+"""
+Copyright (C) 2013 Rice University
+
+This software is subject to the provisions of the GNU AFFERO GENERAL PUBLIC LICENSE Version 3.0 (AGPL).  
+See LICENSE.txt for details.
+"""
+
 import os
 from setuptools import setup, find_packages
 
